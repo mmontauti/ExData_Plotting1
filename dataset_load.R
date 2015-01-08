67b241b23d371ea15e2ccdbd4b18e0b13c838812
Mauro Montauti <mmontauti@users.noreply.github.com>
2015-01-08 15:42:29 -0300

    Code cleanup

--- dataset_load.R
+++ dataset_load.R
@@ -5,12 +5,15 @@ data <- read.table(filename,
                    colClasses = c("character", "character", rep("numeric",7)),
                    na = "?")
 dim(data) 
+
 attach(data)
 subset <- Date == "1/2/2007" | Date == "2/2/2007"
 newData <- data[subset, ]
+
 attach(newData)
 x <- paste(Date, Time)
 newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
 rownames(newData) <- 1:nrow(newData)
 dim(newData)
+
 attach(newData)
