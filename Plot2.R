f23ff6fcfd999bc233f4ef3c206c1e2e6e81bff5
Mauro Montauti <mmontauti@Mauros-MacBook-Pro.local>
2015-01-08 17:01:02 -0300

    Plot2.R Results

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+source("./dataset_load.R")
+
+png(filename = "plot2.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+
